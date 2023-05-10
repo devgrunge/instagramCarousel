5866daeaaf8d2da753712254299ceb15c23cd4d5
Alexander Marcondes Junior <alexmarcjr21@gmail.com>
2023-05-10 13:17:11 +0100

    feat: refresh access token

--- views/js/default.js
+++ views/js/default.js
@@ -1,8 +1,38 @@
-/* insta carousel */
-console.log(location.pathname);
-	if(location.pathname === '/pt/' || location.pathname === '/en/'){
+let userAccessToken = 'USER_ACCESS_TOKEN_HERE';
+
+/* if condition to render this script only in the needed pages */
+if(location.pathname === '/pt/' || location.pathname === '/en/'){
+const token = "TOKEN_HERE";
+		
+function refreshAccessToken() {
+  const APP_ID = 'APP_ID_HERE';
+  const APP_SECRET = 'APP_SECRE_HERE';
+
+  // Get app access token
+  fetch(`https://graph.facebook.com/oauth/access_token?client_id=${APP_ID}&client_secret=${APP_SECRET}&grant_type=client_credentials`)
+    .then(response => response.json())
+    .then(data => {
+      const appAccessToken = data.access_token;
+
+      // Get user access token
+      fetch(`https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${userAccessToken}`)
+        .then(response => response.json())
+        .then(data => {
+          userAccessToken = data.access_token;
+          const expirationTime = data.expires_in;
+
+         /* console.log(`New access token: ${userAccessToken}`); */
+
+          // Schedule next refresh
+          setTimeout(refreshAccessToken, expirationTime * 1000);
+        })
+        .catch(error => console.error(error));
+    })
+    .catch(error => console.error(error));
+}		
+		
 const instagramApi = () => {
-  const token = "YOU_TOKEN_HERE";
+
   const url = `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,thumbnail_url,permalink,timestamp&access_token=${token}`;
 
   fetch(url)
@@ -41,6 +71,13 @@ const instagramApi = () => {
     })
     .catch((e) => console.error(e));
 };
-
+refreshAccessToken();
 instagramApi();
 	}
+
+/* Loader by checkout */
+if (location.pathname === '/pt/encomenda' || location.pathname === '/en/order') {
+    document.getElementsByClassName("btn btn-primary center-block")[0].addEventListener("click", function() {
+        document.getElementById("checkout-finish-loader").style.display = "block";
+    });
+}
